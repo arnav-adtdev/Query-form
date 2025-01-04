abae4375be37206a40c1fe2c80afd281a6522c6f
Arnav <arnavchaturvedi0@gmail.com>
2025-01-04 16:22:01 +0530

    updated code for vercel

--- server.js
+++ server.js
@@ -13,9 +13,12 @@ dotenv.config();
 // Connect to the database
 connectDB();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
   res.render('index'); 
