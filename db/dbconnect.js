7bc09c525336bde1e711b0acdfa5dd569ea1dbba
Arnav <arnavchaturvedi0@gmail.com>
2025-01-04 17:45:49 +0530

    changing  in footer

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected ....');
+    console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
@@ -11,4 +14,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
